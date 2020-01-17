0437ba47844734e4d51eda4ec87b9be92004edda
Richard Lu <sungsta@berkeley.edu>
2020-01-17 11:02:38 -0800

    fix portfolio graph not stacking same brands

--- frontend/components/portfolio/portfolio_graph_values.jsx
+++ frontend/components/portfolio/portfolio_graph_values.jsx
@@ -4,6 +4,37 @@ import isEmpty from '../../utils/obj-util';
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 export default ( {portfolio, sales, products} ) => {
+  let graphData = [];
+  let nike = {y: 0, label: 'Nike'};
+  let jordan = {y: 0, label: 'Jordan'};
+  // console.dir(sales);
+  if (!isEmpty(sales)) {
+    Object.values(portfolio).forEach(item => {
+      // console.dir(item);
+      if (products[item.product_id].brand === 'Nike') {
+        if (sales[item.product_id]) {
+          // console.log('nike');
+          // console.log(item.product_id);
+          // console.log(sales[item.product_id]);
+          nike.y += sales[item.product_id].lastSale.price;
+        } else {
+          nike.y += item.purchase_price;
+        }
+      } else if (products[item.product_id].brand === 'Jordan') {
+        if (sales[item.product_id]) {
+          // console.log('jordan');
+          // console.log(item.product_id);
+          // console.log(sales[item.product_id]);
+          jordan.y += sales[item.product_id].lastSale.price;
+        } else {
+          jordan.y += item.purchase_price;
+        }
+      }
+    });
+  }
+  if (nike.y > 0) graphData.push(nike);
+  if (jordan.y > 0) graphData.push(jordan);
+  // console.dir(graphData);
   const options = {
     theme: "light2",
     animationEnabled: true,
@@ -21,7 +52,7 @@ export default ( {portfolio, sales, products} ) => {
       toolTipContent: "{label}: <strong>${y}</strong>",
       indexLabel: "${y}",
       indexLabelPlacement: "inside",
-      dataPoints: (!isEmpty(sales) ? Object.values(portfolio).map(item => (sales[item.product_id] ? { y: sales[item.product_id].lastSale.price, label: products[item.product_id].brand } : {y: 0, label: ""})) : null)
+      dataPoints: graphData.length ? graphData : null //(!isEmpty(sales) ? Object.values(portfolio).map(item => (sales[item.product_id] ? { y: sales[item.product_id].lastSale.price, label: products[item.product_id].brand } : {y: 0, label: ""})) : null)
       // [
       //   { y: 32, label: "Health" },
       //   { y: 22, label: "Finance" },
