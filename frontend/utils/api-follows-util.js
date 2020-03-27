2b249e645ee8c956873bddd25ec2d408af898d30
Richard Lu <sungsta@berkeley.edu>
2020-03-27 13:31:38 -0700

    create follow util

---
+++ frontend/utils/api-follows-util.js
@@ -0,0 +1,21 @@
+export const fetchFollows = (user_id) => (
+  $.ajax({
+    method: 'GET',
+    url: `/api/users/${user_id}/orders`
+  })
+);
+
+export const createFollow = (follow) => (
+  $.ajax({
+    method: 'POST',
+    url: '/api/follows',
+    data: {follow}
+  })
+);
+
+export const deleteFollow = (id) => (
+  $.ajax({
+    method: 'DELETE',
+    url: `/api/follows/${id}`
+  })
+);
